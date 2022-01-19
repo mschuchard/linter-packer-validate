143d47e869b832deb7222efeeb28ccb2dc2a55c0
Matt Schuchard <grapheneowns@gmail.com>
2022-01-19 16:46:09 -0500

    unit test for error message with extra commas and newlines

---
+++ spec/fixtures/ok_hcl_extra_packer_comma.pkr.hcl
@@ -0,0 +1,3 @@
+packer {
+  required_version = "~> 1.6.0"
+}
--- spec/linter-packer-validate-spec.js
+++ spec/linter-packer-validate-spec.js
@@ -70,6 +70,41 @@ describe('The Packer Validate provider for Linter', () => {
     });
   });
 
+  describe('checks a packer hcl template with an error message with multiple commas and newlines', () => {
+    let editor = null;
+    const badFile = path.join(__dirname, 'fixtures/', 'ok_hcl_extra_packer_comma.pkr.hcl');
+    beforeEach(() => {
+      waitsForPromise(() =>
+        atom.workspace.open(badFile).then(openEditor => {
+          editor = openEditor;
+        })
+      );
+    });
+
+    it('finds the first message', () => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
+          expect(messages.length).toEqual(1);
+        })
+      );
+    });
+
+    it('verifies the message, and replaces the machine commas and newlines with real commas and whitespace', () => {
+      waitsForPromise(() => {
+        return lint(editor).then(messages => {
+          expect(messages[0].severity).toBeDefined();
+          expect(messages[0].severity).toEqual('error');
+          expect(messages[0].excerpt).toBeDefined();
+          expect(messages[0].excerpt).toEqual('This configuration does not support Packer version 1.7.8. To proceed, either choose another supported Packer version or update this version constraint. Version constraints are normally set for good reason, so updating the constraint may lead to other errors or unexpected behavior.');
+          expect(messages[0].location.file).toBeDefined();
+          expect(messages[0].location.file).toMatch(/.+ok_hcl_extra_packer_comma\.pkr\.hcl$/);
+          expect(messages[0].location.position).toBeDefined();
+          expect(messages[0].location.position).toEqual([[1, 0], [1, 1]]);
+        });
+      });
+    });
+  });
+
   it('finds nothing wrong with a valid packer hcl template', () => {
     let editor = null;
     waitsForPromise(() => {
